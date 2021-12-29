c7d41dfed449e2431e966cbdf38e5b3f72691908
Rezende123 <feliperezende.srz@gmail.com>
2021-12-29 20:07:56 -0300

    chore: Iniciando aplicação .NET

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace site_access_manager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
